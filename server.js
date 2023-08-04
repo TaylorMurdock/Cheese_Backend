7924459284d80f7f8702e12c031fac8dcebeddf4
Taylor Murdock <tjmurd4545@gmail.com>
2023-08-04 12:26:49 -0400

    get, post, put, and delete routes

--- server.js
+++ server.js
@@ -14,8 +14,12 @@ const express = require("express");
 //app object
 const app = express();
 
-// mongoose imort
+// mongoose import
 const mongoose = require("mongoose");
+
+// middleware imports
+const cors = require("cors");
+const morgan = require("morgan");
 
 ///////////////////
 // DATABASE CONNECTION
@@ -33,12 +37,68 @@ mongoose.connection
   .on("error", (error) => console.log(error));
 
 ///////////////////
+// MODELS
+///////////////////
+const CheeseSchema = new mongoose.Schema({
+  name: String,
+  countryOfOrigin: String,
+  image: String,
+});
+
+const Cheese = mongoose.model("Cheese", CheeseSchema);
+
+///////////////////
+// MIDDLEWARE
+///////////////////
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+///////////////////
 // ROUTES
 ///////////////////
 
 // test route
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+// CHEESE INDEX ROUTE
+app.get("/cheese", async (req, res) => {
+  try {
+    res.json(await Cheese.find({}));
+  } catch (error) {
+    res.send(400).json(error);
+  }
+});
+
+// CHEESE CREATE ROUTE
+app.post("/cheese", async (req, res) => {
+  try {
+    res.json(await CheeseSchema.create(req.body));
+  } catch (error) {
+    res.send(400).json(error);
+  }
+});
+
+// EDIT CHEESE ROUTE
+app.put("/cheese/:id", async (req, res) => {
+  try {
+    res.json(
+      await Cheese.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+// DELETE CHEESE ROUTE
+app.delete("/cheese/:id", async (req, res) => {
+  try {
+    res.json(await Cheese.findByIdAndRemove(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 ///////////////////
